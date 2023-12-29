85e4955fb5cfb5ff9b5086f4e82a6e3bccbaad11
Alexandre Bastos <xande.dev@outlook.com>
2023-12-29 15:44:21 -0300

    :construction: Iniciando tela de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,18 @@
 export default function LoginPage() {
   return (
-    <main className="flex h-screen w-screen justify-center items-center">
-      <div className="w-80 h-1/2 flex flex-col justify-center items-center bg-blue-700">
-        <h1>Fazer login</h1>
+    <main className="flex min-h-screen w-screen justify-center items-center">
+      <div className="w-80 h-1/2 flex flex-col justify-center items-center rounded-lg border-2 bg-gray-500">
+        <h1 className="text-xl">Fazer login</h1>
         <form action="">
-          <div className="flex flex-col">
+          <div className="flex flex-col m-3">
             <label htmlFor="">E-mail:</label>
             <input type="email" name="" placeholder="Digite seu e-mail" id="" />
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col m-3">
             <label htmlFor="">Senha:</label>
             <input type="password" name="" placeholder="Digite sua senha" id="" />
           </div>
-          <div className="flex justify-center">
+          <div className="flex justify-center mx-3">
             <button type="submit">ENTRAR</button>
           </div>
         </form>
