f6c836864d0d89def2b211217b7c7ef8b5e55f75
Alexandre Bastos <xande.dev@outlook.com>
2023-12-30 09:42:19 -0300

    :construction: página de login, enviando informações de login para o Firebase

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,57 @@
+"use client";
+
+import { useState } from "react";
+import { initFirebase } from "../../../firebase/fireBaseApp";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 export default function LoginPage() {
+  const [userEmail, setUserEmail] = useState("");
+  const [userPwd, setUserPwd] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, userEmail, userPwd)
+      .then((userCredential) => {
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
+  const app = initFirebase();
+  console.log(app);
+
   return (
     <main className="flex min-h-screen w-screen justify-center items-center">
       <div className="w-80 h-1/2 flex flex-col justify-center items-center rounded-lg border-2 bg-gray-500">
         <h1 className="text-xl">Fazer login</h1>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="flex flex-col m-3">
             <label htmlFor="">E-mail:</label>
-            <input type="email" name="" placeholder="Digite seu e-mail" id="" />
+            <input
+              id="useremail"
+              type="email"
+              placeholder="Digite seu e-mail"
+              onChange={(e) => setUserEmail(e.target.value)}
+            />
           </div>
           <div className="flex flex-col m-3">
             <label htmlFor="">Senha:</label>
-            <input type="password" name="" placeholder="Digite sua senha" id="" />
+            <input
+              id="userpwd"
+              type="password"
+              placeholder="Digite sua senha"
+              onChange={(e) => setUserPwd(e.target.value)}
+            />
           </div>
           <div className="flex justify-center mx-3">
-            <button type="submit">ENTRAR</button>
+            <button>Criar conta</button>
           </div>
         </form>
       </div>
     </main>
-  )
+  );
 }
