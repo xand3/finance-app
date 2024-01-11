3de29476544cba615fa8e24b130e009306b87b91
Alexandre Bastos <xande.dev@outlook.com>
2024-01-11 10:13:30 -0300

    :construction: Incluindo Favicon a página

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,31 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Righteous } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Righteous } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 const righteous = Righteous({
   style: "normal",
-  weight: '400',
-  subsets: ["latin"]
-})
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Finanças',
-  description: 'Gerenciador de Finanças',
-}
+  title: "Finanças",
+  description: "Gerenciador de Finanças",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-br">
-      <body className={`${inter.className}` }>{children}</body>
+      <head>
+        <link rel="icon" href="/favicon/favicon.ico" />
+      </head>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
-  )
+  );
 }
